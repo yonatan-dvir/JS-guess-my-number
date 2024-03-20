1b58105caa5d3c5f71864925c87c5c2ee9112f17
Yonatan Dvir <64538919+yonatan-dvir@users.noreply.github.com>
2024-03-20 20:56:11 +0200

    Added the logic of the guessing game

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-'use strict';
+"use strict";
+
+const randomNum = Math.trunc(Math.random() * 20) + 1;
+
+document.querySelector(".check").addEventListener("click", () => {
+  const guess = Number(document.querySelector(".guess").value);
+
+  // If the user clicked check but didn't guess a number, tell him to guess one.
+  if (!guess) {
+    document.querySelector(".message").textContent = "Please guess a number!";
+  }
+
+  // If the guess is too high
+  else if (guess > randomNum) {
+    document.querySelector(".message").textContent = "Too high! Try again.. ):";
+  }
+
+  // If the guess is too low
+  else if (guess < randomNum) {
+    document.querySelector(".message").textContent = "Too low! Try again.. ):";
+  }
+
+  // Else, the user succeed!
+  else {
+    document.querySelector(".message").textContent = "Correct! You did it (:";
+    document.querySelector(".number").textContent = randomNum;
+  }
+});
